65f69cb902be95ee8ea55be8b798f30987587967
jonjaira <jonjaira@gmail.com>
2020-05-14 16:56:55 -0400

    Jessica's commit

--- src/Mearns/Main.java
+++ src/Mearns/Main.java
@@ -3,6 +3,8 @@ package Mearns;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Taylor");
+	    String teamMate = "Jessica";
+        System.out.println("Hello, my name is Taylor");
+        System.out.println("Hello Taylor my name is " + teamMate);
     }
 }
