79da3668c5293423147ad6774604c371dcc0d047
filipecamargos <filipe.atrabalho@gmail.com>
2020-05-15 11:11:22 -0600

    Hello From Filipe Commit

--- src/Mearns/Main.java
+++ src/Mearns/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	    String teamMate = "Jessica";
         System.out.println("Hello, my name is Taylor");
         System.out.println("Hello Taylor my name is " + teamMate);
+        System.out.println("Hello Taylor and " + teamMate + " my name is Filipe");
     }
 }
